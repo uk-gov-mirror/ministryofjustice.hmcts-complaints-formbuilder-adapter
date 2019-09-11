73ca3e78e95d55c61b6c963798269a5405904bc2
cob16 <public+github@cormacbrady.info>
2019-09-11 12:01:49 +0100

    Request spec and invoke CreateCase when a request comes in
    This will take the request from the submitter, transform it and forward it on to
    Optics.

--- app/controllers/complaint_controller.rb
+++ app/controllers/complaint_controller.rb
@@ -1,5 +1,22 @@
 class ComplaintController < ApplicationController
   def create
-    render json: { placeholder: true }, status: 201
+    Usecase::Optics::CreateCase.new(
+      optics_gateway: Gateway::Optics.new,
+      presenter: OpenStruct.new(present: JSON.parse(@decrypted_body)), # temporary stub presenter
+      get_bearer_token: bearer_token
+    ).execute
+  end
+
+  private
+
+  def bearer_token
+    Usecase::Optics::GetBearerToken.new(
+      optics_gateway: Gateway::Optics.new,
+      generate_jwt_token: Usecase::Optics::GenerateJwtToken.new(
+        url: 'https://uat.icasework.com/token?db=hmcts',
+        api_key: ENV.fetch('OPTICS_API_KEY'),
+        hmac_secret: ENV.fetch('OPTICS_SECRET_KEY')
+      )
+    )
   end
 end
