6f38ad162fd7c86457c45a7624a5e2bf18bd1550
Emile <emile.swarts123@gmail.com>
2019-09-11 16:00:51 +0100

    Restore secret_key_base
    This was removed but is causing an error when health probes hit /health

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
+  config.require_master_key = false
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
