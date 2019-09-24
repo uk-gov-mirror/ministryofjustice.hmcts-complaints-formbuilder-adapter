2d951b5c5350819c4ca2c3638892ee2ccbd5d7c6
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-09-24 13:40:45 +0100

    rubocop fixes

--- spec/usecase/download_attachment_spec.rb
+++ spec/usecase/download_attachment_spec.rb
@@ -8,17 +8,20 @@ RSpec.describe Usecase::DownloadAttachment do
 
     let(:attachment_double) { instance_double(Attachment, url: 'https://example.com.com/hello.txt') }
 
-    it 'returns downloaded file' do
-      stub_request(:get, "https://example.com.com/hello.txt").
-        with(
+    before do
+      stub_request(:get, 'https://example.com.com/hello.txt')
+        .with(
           headers: {
-            'Accept'=>'*/*',
-            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'Host'=>'example.com.com',
-            'User-Agent'=>'Ruby'
-          }).
-          to_return(status: 200, body: file_fixture('hello.txt').read, headers: {})
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Host' => 'example.com.com',
+            'User-Agent' => 'Ruby'
+          }
+        )
+        .to_return(status: 200, body: file_fixture('hello.txt').read, headers: {})
+    end
 
+    it 'returns downloaded file' do
       allow(Attachment).to receive(:find_by).with(identifier: identifier).and_return(attachment_double)
 
       expect(download_attachment.call).to eql("hello world\n")
